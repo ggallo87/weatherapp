2783ce7b20982642d5614bf1e716a08d938a87bc
Gonzalo Martin Gallo <gonzalo.gallo@claro.com.ar>
2019-10-23 14:47:43 -0300

    se crea primera version de weatherApp

---
+++ src/components/WeatherData.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WeatherData = () => (
+    <div>
+        weater Data
+    </div>
+);
+
+export default WeatherData;
---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+
+const weatherLocation = () => (
+    <div>
+        <Location/>
+        <WeatherData/>
+    </div>
+);
+
+export default weatherLocation;
